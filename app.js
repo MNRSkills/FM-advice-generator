6114dc2ccc131f5a906c838781a4961bec5872b4
MNR_Dev <mnrdev510@gmail.com>
2022-03-07 22:57:17 -0800

    fetched the data keeping things simple

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const card = document.getElementById("card-container");
-const adviceHeader = document.getElementsByClassName("advice-header")
 
-const fetchedData = fetch("	https://api.adviceslip.com/advice");
-console.log("DATA FROM ADVICE", fetchedData.then(res => {
-    return res.json()
-}).then(
-    advice => {
-        console.log("LOOKING FOR ADVICE", advice);
-    }
-))
+
+async function adviceAPI() {
+  const h1 = document.createElement("h1");
+  const advice = "https://api.adviceslip.com/advice";
+  try {
+    const fetching = await fetch(advice);
+    const response = await fetching.json();
+    const adviced = await response.slip.advice;
+    card.appendChild(h1);
+    h1.innerHTML = adviced;
+  } catch (error) {
+    console.log(`THERE WAS AN ERROR, ${error}`);
+  }
+}
+
+adviceAPI();
