542e22e269fd76f5cd4f2fa9b25202feca1dbeae
MNR_Dev <mnrdev510@gmail.com>
2022-03-13 17:19:20 -0700

    added final touches with fetch and styles

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const card = document.getElementById("card-container");
+const card = document.getElementById("card-advice");
 
 
 async function adviceAPI() {
+  const p = document.createElement("p")
   const h1 = document.createElement("h1");
   const advice = "https://api.adviceslip.com/advice";
   try {
     const fetching = await fetch(advice);
     const response = await fetching.json();
-    const adviced = await response.slip.advice;
+    console.log("HOW MANY Q", response);
+    const adviced = await ' " '+ response.slip.advice + ' " ';
+    const quoteNum = await "ADVICE #" + response.slip.id;
+    card.appendChild(p)
     card.appendChild(h1);
+    p.innerHTML = quoteNum;
     h1.innerHTML = adviced;
   } catch (error) {
     console.log(`THERE WAS AN ERROR, ${error}`);
